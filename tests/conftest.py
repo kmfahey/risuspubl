1e1e4451cced796c3c327cb062867f495943ef12
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-09-16 16:04:16 -0700

    implemented 2 further tests for /authors endpoints; only 80 to go

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
 import os
+import warnings
 
 import pytest
 import psycopg2
-import flask
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, upgrade, downgrade
-
 import risuspubl.api.authors
 import risuspubl.api.books
 import risuspubl.api.clients
@@ -23,49 +21,83 @@ from risuspubl.flaskapp import create_app
 
 
 # Set environment variable for Flask's configuration
-os.environ["FLASK_ENV"] = "testing"  # This should be set before creating the app instance.
+os.environ[
+    "FLASK_ENV"
+] = "testing"  # This should be set before creating the app instance.
+
 
 def pytest_sessionstart(session):
     # PostgreSQL server details
-    HOST = 'localhost'
+    HOST = "localhost"
     PORT = 5432
-    TARGET_DATABASE = 'risusp_test'
-    TARGET_USER = 'pguser'
-    TARGET_PASSWORD = 'pguser'  # Be careful with storing plaintext passwords!
+    TARGET_DATABASE = "risusp_test"
+    TARGET_USER = "pguser"
+    TARGET_PASSWORD = "pguser"  # Be careful with storing plaintext passwords!
 
     # Check for user existence by attempting to connect with the provided credentials
     try:
         # This connection will just use the default 'postgres' database
-        conn = psycopg2.connect(dbname='postgres', user=TARGET_USER, password=TARGET_PASSWORD, host=HOST, port=PORT)
+        conn = psycopg2.connect(
+            dbname="postgres",
+            user=TARGET_USER,
+            password=TARGET_PASSWORD,
+            host=HOST,
+            port=PORT,
+        )
     except psycopg2.OperationalError as e:
-        if "password authentication failed" in str(e) or "role" in str(e) and "does not exist" in str(e):
-            pytest.exit(f"The user '{TARGET_USER}' does not exist or wrong password provided. Please run ansible with setup_playbook.yml.")
+        if (
+            "password authentication failed" in str(e)
+            or "role" in str(e)
+            and "does not exist" in str(e)
+        ):
+            pytest.exit(
+                f"The user '{TARGET_USER}' does not exist or wrong password provided. Please run ansible with setup_playbook.yml."
+            )
         pytest.exit(f"Failed to connect to the PostgreSQL server. Error: {str(e)}")
 
     # If the above connection succeeded, the user exists. Now, check for the database's existence.
     try:
         with conn.cursor() as cursor:
-            cursor.execute(f"SELECT 1 FROM pg_database WHERE datname='{TARGET_DATABASE}'")
+            cursor.execute(
+                f"SELECT 1 FROM pg_database WHERE datname='{TARGET_DATABASE}'"
+            )
             if not cursor.fetchone():
-                pytest.exit(f"The test database '{TARGET_DATABASE}' does not exist. Please run ansible with setup_playbook.yml.")
+                pytest.exit(
+                    f"The test database '{TARGET_DATABASE}' does not exist. Please run ansible with setup_playbook.yml."
+                )
     finally:
         conn.close()
 
 
-@pytest.fixture(scope='session')
-def db_setup_teardown():
+@pytest.fixture
+def fresh_tables_db():
+    yield db
+
+    meta = db.metadata
+    for table in reversed(meta.sorted_tables):
+        db.session.execute(table.delete())
+    db.session.commit()
+
+
+@pytest.fixture(scope="session")
+def staged_app_client():
     # Create the Flask app instance using the test config
-    app = create_app(dict(SQLALCHEMY_DATABASE_URI = f"postgresql://pguser:pguser@localhost:5432/risusp_test"))
+    app = create_app(
+        dict(
+            SQLALCHEMY_DATABASE_URI=f"postgresql://pguser:pguser@localhost:5432/risusp_test"
+        )
+    )
     app_context = app.app_context()
     app_context.push()
 
-    # Initialize Flask-Migrate with the app instance and db
-    migrate = Migrate(app, db, directory='./alembic')
-
     with app.app_context():
-        upgrade()
+        db.create_all()
 
-        yield app
+        with app.test_client() as client:
+            yield app, client
 
-        downgrade()
+        db.session.commit()
+        db.session.close()
+
+        db.drop_all()
 
--- tests/test_authors.py
+++ tests/test_authors.py
@@ -1,35 +1,96 @@
 #!/usr/bin/python3
 
 import os
+import random
 
+import faker
 import pytest
-import flask
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, upgrade, downgrade
-
-import risuspubl.api.authors
-import risuspubl.api.books
-import risuspubl.api.clients
-import risuspubl.api.editors
-import risuspubl.api.manuscripts
-import risuspubl.api.sales_records
-import risuspubl.api.salespeople
-import risuspubl.api.series
-
-from risuspubl.dbmodels import db
-from risuspubl.flaskapp import create_app
-
+from sqlalchemy.exc import LegacyAPIWarning
+from risuspubl.dbmodels import Author, Editor, Book, Series
 
 
 # Set environment variable for Flask's configuration
-os.environ["FLASK_ENV"] = "testing"  # This should be set before creating the app instance.
+os.environ[
+    "FLASK_ENV"
+] = "testing"  # This should be set before creating the app instance.
+
+faker_obj = faker.Faker()
 
 
-def test_author_create_endpoint(db_setup_teardown):
-    pass
+def test_author_create_endpoint(fresh_tables_db, staged_app_client):
+    db = fresh_tables_db
+    app, client = staged_app_client
+    author_dict = dict(
+        first_name=faker_obj.first_name(), last_name=faker_obj.last_name()
+    )
+    response = client.post("/authors", json=author_dict)
+    assert response.status_code == 200
+    author_objs = db.session.query(Author).all()
+    assert len(author_objs) == 1
+    assert author_objs[0].first_name == author_dict["first_name"]
+    assert author_objs[0].last_name == author_dict["last_name"]
 
-# def test_create_author_book_endpoint
+
+def test_create_author_book_endpoint(fresh_tables_db, staged_app_client):
+    db = fresh_tables_db
+    app, client = staged_app_client
+
+    def _setup():
+        author_obj = Author(
+            first_name=faker_obj.first_name(), last_name=faker_obj.last_name()
+        )
+        db.session.add(author_obj)
+        editor_obj = Editor(
+            first_name=faker_obj.first_name(),
+            last_name=faker_obj.last_name(),
+            salary=random.randint(65, 95) * 1000,
+        )
+        db.session.add(editor_obj)
+        db.session.commit()
+
+        book_dict = dict(
+            title="The Accidental Wedding",
+            publication_date=faker_obj.date_this_century().isoformat(),
+            edition_number=random.randint(1, 5),
+            is_in_print=bool(random.randint(0, 1)),
+            editor_id=editor_obj.editor_id,
+        )
+
+        return author_obj.author_id, book_dict
+
+    def _test(response, book_dict):
+        assert response.status_code == 200
+        resp_jsobj = response.get_json()
+        assert resp_jsobj["edition_number"] == book_dict["edition_number"]
+        assert resp_jsobj["editor_id"] == book_dict["editor_id"]
+        assert resp_jsobj["is_in_print"] == book_dict["is_in_print"]
+        assert resp_jsobj["publication_date"] == book_dict["publication_date"]
+        assert resp_jsobj["title"] == book_dict["title"]
+        return resp_jsobj
+
+    author_id, book_dict = _setup()
+
+    # Testing without series_id
+    response = client.post(f"/authors/{author_id}/books", json=book_dict)
+    resp_jsobj = _test(response, book_dict)
+    assert resp_jsobj["series_id"] is None
+
+    # Cleanup
+    for table in reversed(db.metadata.sorted_tables):
+        db.session.execute(table.delete())
+
+    # Testing with series_id
+    author_id, book_dict = _setup()
+
+    series_obj = Series(title="Sign of the Absent Puppet", volumes=random.randint(1, 5))
+    db.session.add(series_obj)
+    db.session.commit()
+    book_dict["series_id"] = series_obj.series_id
+
+    response = client.post(f"/authors/{author_id}/books", json=book_dict)
+    resp_jsobj = _test(response, book_dict)
+    assert resp_jsobj["series_id"] == book_dict["series_id"], (resp_jsobj, book_dict)
+
 
 # def test_create_author_manuscript_endpoint
 
@@ -73,7 +134,26 @@ def test_author_create_endpoint(db_setup_teardown):
 
 # def test_display_authors_manuscripts_endpoint
 
-# def test_index_endpoint
+
+def test_index_endpoint(fresh_tables_db, staged_app_client):
+    db = fresh_tables_db
+    app, client = staged_app_client
+    author_dicts = [
+        dict(first_name=faker_obj.first_name(), last_name=faker_obj.last_name())
+        for _ in range(10)
+    ]
+    for author_dict in author_dicts:
+        db.session.add(Author(**author_dict))
+    db.session.commit()
+    response = client.get("/authors")
+    assert response.status_code == 200
+    for author_jsobj in response.get_json():
+        assert any(
+            author_dict["first_name"] == author_jsobj["first_name"]
+            and author_dict["last_name"] == author_jsobj["last_name"]
+            for author_dict in author_dicts
+        )
+
 
 # def test_update_author_book_endpoint
 
