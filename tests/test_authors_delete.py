d7b5571e50fcf06fa2a64209496a73a173c6e73a
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-09-20 22:54:42 -0700

    added test for DELETE /authors/<id>/manuscrits/<id>

--- tests/test_authors_delete.py
+++ tests/test_authors_delete.py
@@ -104,7 +104,51 @@ def test_delete_author_by_id_endpoint(db_w_cleanup, staged_app_client):  # 8/83
     assert response.status_code == 404
 
 
-# def test_delete_author_manuscript_endpoint # 9/83
+def test_delete_author_manuscript_endpoint(db_w_cleanup, staged_app_client):  # 9/83
+    db = db_w_cleanup
+    app, client = staged_app_client
+
+    author_obj = Genius.gen_author_obj()
+    manuscript_obj = Genius.gen_manuscript_obj()
+    manuscript_id = manuscript_obj.manuscript_id
+    authors_manuscripts_obj = Genius.gen_authors_manuscripts_obj(
+        author_obj.author_id, manuscript_id
+    )
+
+    response = client.delete(
+        f"/authors/{author_obj.author_id}/manuscripts/{manuscript_id}"
+    )
+    assert response.status_code == 200
+    assert json.loads(response.data) is True
+    assert db.session.query(Author).get(author_obj.author_id) is not None
+    assert db.session.query(Manuscript).get(manuscript_id) is None
+    assert (
+        db.session.query(AuthorsManuscripts)
+        .filter_by(author_id=author_obj.author_id, manuscript_id=manuscript_id)
+        .first()
+        is None
+    )
+
+    DbBasedTester.cleanup__empty_all_tables()
+
+    manuscript_obj = Genius.gen_manuscript_obj()
+    bogus_author_id = random.randint(1, 10)
+    response = client.delete(
+        f"/authors/{bogus_author_id}/manuscripts/{manuscript_obj.manuscript_id}"
+    )
+    assert response.status_code == 404
+    assert db.session.query(Manuscript).get(manuscript_obj.manuscript_id) is not None
+
+    DbBasedTester.cleanup__empty_all_tables()
+
+    author_obj = Genius.gen_author_obj()
+    bogus_manuscript_id = random.randint(1, 10)
+    response = client.delete(
+        f"/authors/{author_obj.author_id}/manuscripts/{bogus_manuscript_id}"
+    )
+    assert response.status_code == 404
+    assert db.session.query(Author).get(author_obj.author_id) is not None
+
 
 # def test_delete_author_metadata_endpoint # 10/83
 
