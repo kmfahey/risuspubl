3e0faa14fc54f94c6e249ae5e4ddc8b2dd84692b
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-09-25 22:26:56 -0700

    added test for GET /sales_records/books/<id>

--- tests/test_sales_records.py
+++ tests/test_sales_records.py
@@ -31,20 +31,48 @@ def test_display_sales_record_endpoint(db_w_cleanup, staged_app_client):  # 67/8
     response = client.get(f"/sales_records/{sales_record_obj.sales_record_id}")
     DbBasedTester.test_sales_record_resp(response, sales_record_obj)
 
+    DbBasedTester.cleanup__empty_all_tables()
 
-#    DbBasedTester.cleanup__empty_all_tables()
-#
-#    # Testing for 404 error when called with a bogus book_id
-#    bogus_book_id = random.randint(1, 10)
-#    response = client.get(f"/books/{bogus_book_id}")
-#    assert response.status_code == 404, response.data
+    # Testing for 404 error when called with a bogus sales_record_id
+    bogus_sales_record_id = random.randint(1, 10)
+    response = client.get(f"/sales_records/{bogus_sales_record_id}")
+    assert response.status_code == 404, response.data
 
 
-## Testing GET /sales_records/books/<id> endpoint
-# def test_display_sales_records_by_book_id_endpoint(db_w_cleanup, staged_app_client): # 68/83
-#    db = db_w_cleanup
-#    app, client = staged_app_client
-#
+# Testing GET /sales_records/books/<id> endpoint
+def test_display_sales_records_by_book_id_endpoint(
+    db_w_cleanup, staged_app_client
+):  # 68/83
+    db = db_w_cleanup
+    app, client = staged_app_client
+
+    editor_obj = Genius.gen_editor_obj()
+    book_obj = Genius.gen_book_obj(editor_obj.editor_id)
+    sales_record_objs_l = [
+        Genius.gen_sales_record_obj(book_obj.book_id) for _ in range(10)
+    ]
+    response = client.get(f"/sales_records/books/{book_obj.book_id}")
+
+    assert response.status_code == 200
+    for sales_record_jsobj in response.get_json():
+        assert any(
+            sales_record_jsobj["year"] == sales_record_obj.year
+            and sales_record_jsobj["month"] == sales_record_obj.month
+            and sales_record_jsobj["copies_sold"] == sales_record_obj.copies_sold
+            and sales_record_jsobj["gross_profit"]
+            == float(sales_record_obj.gross_profit)
+            and sales_record_jsobj["net_profit"] == float(sales_record_obj.net_profit)
+            for sales_record_obj in sales_record_objs_l
+        )
+
+    DbBasedTester.cleanup__empty_all_tables()
+
+    # Testing for 404 error when called with a bogus sales_record_id
+    bogus_book_id = random.randint(1, 10)
+    response = client.get(f"/sales_record/books/{bogus_book_id}")
+    assert response.status_code == 404, response.data
+
+
 ## Testing GET /sales_records/years/<year>/books/<month> endpoint
 # def test_display_sales_records_by_year_and_book_id_endpoint(db_w_cleanup, staged_app_client): # 69/83
 #    db = db_w_cleanup
